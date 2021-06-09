cb71ce16034690ddd3c6e5e579cda68e93fcda9c
nimaghazanfari <ghazanfari_nima@yahoo.com>
2021-06-09 16:50:33 +0430

    test cases added

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,44 +2,95 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Escrow is Ownable {
+contract Escrow is Ownable, AccessControl {
     using SafeMath for uint256;
     using Address for address payable;
 
-    address public buyer;
-    address public seller;
-    uint256 public percent;
-    uint256 public totalPrice;
+    bytes32 public constant SYSTEM_AGENT = keccak256("SYSTEM_AGENT");
+    bytes32 public constant SYSTEM_BUYER = keccak256("SYSTEM_BUYER");
+    bytes32 public constant SYSTEM_SELLER = keccak256("SYSTEM_SELLER");
 
-    function initEscrow(
+    enum EscrowStatus {FOR_SALE, SOLD, WITHDRAWN}
+    EscrowItem[] public escrows;
+    mapping(uint256 => bool) public contains;
+
+    struct EscrowItem {
+        address buyer;
+        address seller;
+        uint256 percent;
+        uint256 totalPrice;
+        uint256 status;
+    }
+
+    modifier onlySeller() {
+        //checking Role == SELLER
+        require(hasRole(SYSTEM_SELLER, _msgSender()), "!seller");
+        _;
+    }
+
+    function initNewEscrowItem(
         address _seller,
         uint256 _totalPrice,
         uint256 _percent
     ) external onlyOwner {
         require(_percent < 100, "!all");
 
-        seller = _seller;
-        totalPrice = _totalPrice;
-        percent = _percent;
+        EscrowItem memory newItem =
+            EscrowItem({
+                buyer: address(0),
+                seller: _seller,
+                percent: _percent,
+                totalPrice: _totalPrice,
+                status: uint256(EscrowStatus.FOR_SALE)
+            });
+
+        _setupRole(SYSTEM_SELLER, _seller);
+
+        escrows.push(newItem);
+        contains[escrows.length - 1] = true;
     }
 
-    function buyItem(address _buyer) external payable onlyOwner {
+    function buyItem(uint256 _itemId) external payable {
+        require(contains[_itemId], "!exists");
+        EscrowItem storage item = escrows[_itemId];
+
+        require(item.status == uint256(EscrowStatus.FOR_SALE), "!for-sale");
+
         //buyer must send the exact price defined by the seller
-        require(msg.value == totalPrice, "!totalPrice");
-        buyer = _buyer;
+        require(msg.value == item.totalPrice, "!totalPrice");
+        item.buyer = _msgSender();
+        item.status = uint256(EscrowStatus.SOLD);
+
+        //refund extra payment
+        if (msg.value > item.totalPrice) {
+            uint256 diff = msg.value.sub(item.totalPrice);
+            payable(item.buyer).sendValue(diff);
+        }
     }
 
-    function withdraw() external onlyOwner {
+    function withdraw(uint256 _itemId) external onlySeller {
+        require(contains[_itemId], "!exists");
+        EscrowItem storage item = escrows[_itemId];
+
+        require(item.seller == _msgSender(), "!ownseller");
+        require(item.status != uint256(EscrowStatus.FOR_SALE), "!sold");
+        require(item.status != uint256(EscrowStatus.WITHDRAWN), "withdrawn");
+
         uint256 totalDeposit = address(this).balance;
         require(totalDeposit > 0, "!balance");
 
-        uint256 agentFee = totalDeposit.mul(percent).div(100);
+        uint256 agentFee = item.totalPrice.mul(item.percent).div(100);
         require(agentFee > 0, "!fee");
 
+        item.status = uint256(EscrowStatus.WITHDRAWN);
         //subtract the agent's fee, and send the rest to the seller
-        payable(seller).sendValue(totalDeposit.sub(agentFee));
+        uint256 amount = item.totalPrice.sub(agentFee);
+        require(amount > 0, "!fee");
+        
+        payable(item.seller).sendValue(amount);
     }
 }
--- test/1_escrowgate.test.js
+++ test/1_escrowgate.test.js
@@ -12,9 +12,10 @@ contract('Escrow', async (accounts) => {
     let agent;
     let alice;
     let bob;
+    let jack;
 
     before(async () => {
-        [agent, alice, bob] = accounts;
+        [agent, alice, bob, jack] = accounts;
         instance = await Escrow.deployed();
     });
 
@@ -22,10 +23,107 @@ contract('Escrow', async (accounts) => {
 
         it('should init escrow', async () => {
 
-            await instance.initEscrow(alice, 50000, 5);
+            //initiate new item to be sold
+            await instance.initNewEscrowItem(alice, web3.utils.toWei('1', 'ether'), 5);            
+            const exists = await instance.contains(0);
+            expect(exists).to.be.true;
 
-        })
+        });
 
-    })
+    });
 
-})
+    describe('Buying', () => {
+
+        it('should revert for non-existant items', async () => {
+
+            await expect(instance.buyItem(2)).to.be.revertedWith('!exists');
+
+        });
+
+        it('should revert when payment < totalPrice', async () => {
+
+            await expect(instance.buyItem(0, {
+                from: bob, value: 2000
+            })).to.be.revertedWith('!totalPrice');
+
+        });
+
+        it('should buy an item', async () => {
+
+            const paymentAmount = web3.utils.toWei('1', 'ether');
+
+            //buy item[0]
+            await instance.buyItem(0, { from: bob, value: paymentAmount });
+            const item = await instance.escrows(0);
+            expect(item.buyer).to.equal(bob);
+
+            //check total balance after payment
+            const totalBalance = await web3.eth.getBalance(instance.address);
+            expect(totalBalance).to.equal(`${paymentAmount}`);
+
+        });
+
+        it('should revert for SOLD items', async () => {
+
+            const paymentAmount = web3.utils.toWei('1', 'ether');
+
+            //buy item[0] again
+            await expect(instance.buyItem(0, {
+                from: bob, value: paymentAmount
+            })).to.be.revertedWith('!for-sale');
+
+        });
+    });
+
+    describe('Withdrawal', () => {
+
+
+        it('should revert if RBAC reverts', async () => {
+
+            //alice is the seller
+            await expect(instance.withdraw(0, { from: bob })).to.be.revertedWith('!seller');
+
+        });
+
+        it('should revert for non-existant items', async () => {
+
+            await expect(instance.withdraw(2, { from: alice })).to.be.revertedWith('!exists');
+
+        });
+
+        it('should revert for not SOLD items', async () => {
+
+            //adding new Item
+            await instance.initNewEscrowItem(alice, 80000, 10);
+            await expect(instance.withdraw(1, { from: alice })).to.be.revertedWith('!sold');
+
+        });
+
+        it('should revert for not Own Seller', async () => {
+
+            //jack is the new escrow seller
+            await instance.initNewEscrowItem(jack, 60000, 10);
+            await instance.buyItem(2, { from: bob, value: 60000 });
+            await expect(instance.withdraw(2, { from: alice })).to.be.revertedWith('!ownseller');
+
+        });
+
+        it('should withdraw for seller', async () => {
+
+            const balanceBefore = await web3.eth.getBalance(alice);
+            await instance.withdraw(0, { from: alice });
+            const balanceAfter = await web3.eth.getBalance(alice);
+
+            const BN = web3.utils.BN;
+            expect(new BN(balanceAfter) > new BN(balanceBefore)).to.be.true;
+        });
+
+        it('should revert for WITHDRAWN items', async () => {
+
+            //item[0] is already withdrawn
+            await expect(instance.withdraw(0, { from: alice })).to.be.revertedWith('withdrawn');
+
+        });
+    });
+
+});
